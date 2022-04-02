9517ad1c008b84e1bb8b42cde2099ab61084c23b
demon <demon@DESKTOP-83N8TQ9>
2022-04-02 10:25:30 +0700

    Add project files.

---
+++ Baithuchanhso1_HelloWindow/Baithuchanhso1_HelloWindow.cpp
@@ -0,0 +1,98 @@
+﻿// Baithuchanhso1_HelloWindow.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/////Phần 1. Thêm các thư viện
+//GLEW
+
+#include <GL/glew.h>
+//GLFW
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+////Phần 2. Khai báo các hằng số, biến toàn cục, định nghĩa Hàm/Phương thức
+//function
+void key_callback1(GLFWwindow* window, int key, int scancode, int action, int mode);
+//kích thước window
+const GLuint _WIDTH = 800, _HEIGHT = 600;
+
+/// <summary>
+/// Phần 3 :Hàm Main, mình bắt đầu ứng dụng tại đây và chạy Game Loop.
+/// </summary>
+/// <returns></returns>
+int main()
+{
+	//Init GLFW (khởi tạo GLFW)
+	glfwInit();
+	// Cấu hình các cài đặt yêu cầu cho GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	//Tạo một đối tượng GLFWwindow 
+	GLFWwindow* window = glfwCreateWindow(_WIDTH, _HEIGHT, "Hello window 64IT", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	//set key từ bàn phím
+	glfwSetKeyCallback(window, key_callback1);
+
+	//Khởi tạo GLEW
+	//đặt giá trị cho glewExperimental= GL_TRUE trước khi khở tạo GLEW
+	glewExperimental = GL_TRUE;
+	
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	//Đinh nghĩa kích thước viewport
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	//Vòng lặp trò chơi GAME LOOP
+	while (!glfwWindowShouldClose(window))
+	{
+		//kiểm tra nếu có bất kỳ sự kiện nào được kích hoạt (key bàn phím, chuột,etc) và gọi các function
+		glfwPollEvents();
+
+		//Rendering
+		//xóa color buffer
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f); 
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+
+
+
+		//swap the screen buffers 
+		glfwSwapBuffers(window);
+	}
+
+
+
+
+
+	// cuối cùng, giải phóng tài nguyên GLFW
+	glfwTerminate();
+	return 0;
+}
+
+
+
+////Phần 4: bổ sung các hàm/ phương thức
+void key_callback1(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	std::cout << key << std::endl;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{ 
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
+
---
+++ Baithuchanhso3_Shader_ClassShader/Shader.h
@@ -0,0 +1,170 @@
+﻿#ifndef SHADER1_H
+#define SHADER1_H
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+using namespace std;
+#include <GL/glew.h>;
+#include <glm/glm.hpp>
+
+class Shader
+{
+public:
+	//thuộc tính
+	//ID Program
+	GLuint IDProgram;
+	
+
+	//khởi tạo constructor 
+	Shader(const char* vertexPath, const char* fragmentPath)
+	{
+		//1. Lấy đường dẫn dẫn vertex/fragment source code từ file  *.vs *.frag
+		std::string vertexCode;
+		std::string fragmentCode;
+		std::ifstream vShaderFile;
+		std::ifstream fShaderFile;
+		// đảm bảo ifstream có thể thông qua các ngoại lệ
+		vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		try
+		{
+			//openfile
+			vShaderFile.open(vertexPath);
+			fShaderFile.open(fragmentPath);
+			std::stringstream vShaderStream, fShaderStream;
+			//read file và lưu vào bộ nhớ
+			vShaderStream << vShaderFile.rdbuf();
+			fShaderStream << fShaderFile.rdbuf();
+			//close file
+			vShaderFile.close();
+			fShaderFile.close();
+			// chuyển đổi stream thành string
+			vertexCode = vShaderStream.str();
+			fragmentCode = fShaderStream.str();
+		}
+		catch (std::ifstream::failure& e)
+		{
+			std::cout << "ERROR::SHADER::FILE NOT SUCCESFULLY READ" << std::endl;
+		}
+
+		const char* vShaderCode = vertexCode.c_str();
+		const char* fShaderCode = fragmentCode.c_str();
+
+		//2. Biên dịch (Compile) Shader 
+		GLuint vertexShader, fragmentShader;
+		//Vertex Shader
+		vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertexShader, 1, &vShaderCode, NULL);
+		glCompileShader(vertexShader);
+		CheckCompileError(vertexShader, "VERTEX");
+		//fragment Shader
+		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragmentShader, 1, &fShaderCode, NULL);
+		glCompileShader(fragmentShader);
+		CheckCompileError(fragmentShader, "FRAGMENT");
+		//shader program
+		IDProgram = glCreateProgram();
+		glAttachShader(IDProgram, vertexShader);
+		glAttachShader(IDProgram, fragmentShader);
+		glLinkProgram(IDProgram);
+		CheckCompileError(IDProgram, "PROGRAM");
+		// delete shader
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+	}
+	//active shader
+	void use()
+	{
+		glUseProgram(IDProgram);
+	}
+	//uniform
+	// utility uniform functions
+	// ------------------------------------------------------------------------
+	void setBool(const std::string& name, bool value) const
+	{
+		glUniform1i(glGetUniformLocation(IDProgram, name.c_str()), (int)value);
+	}
+	// ------------------------------------------------------------------------
+	void setInt(const std::string& name, int value) const
+	{
+		glUniform1i(glGetUniformLocation(IDProgram, name.c_str()), value);
+	}
+	// ------------------------------------------------------------------------
+	void setFloat(const std::string& name, float value) const
+	{
+		glUniform1f(glGetUniformLocation(IDProgram, name.c_str()), value);
+	}
+	// ------------------------------------------------------------------------
+	void setVec2(const std::string& name, const glm::vec2& value) const
+	{
+		glUniform2fv(glGetUniformLocation(IDProgram, name.c_str()), 1, &value[0]);
+	}
+	void setVec2(const std::string& name, float x, float y) const
+	{
+		glUniform2f(glGetUniformLocation(IDProgram, name.c_str()), x, y);
+	}
+	// ------------------------------------------------------------------------
+	void setVec3(const std::string& name, const glm::vec3& value) const
+	{
+		glUniform3fv(glGetUniformLocation(IDProgram, name.c_str()), 1, &value[0]);
+	}
+	void setVec3(const std::string& name, float x, float y, float z) const
+	{
+		glUniform3f(glGetUniformLocation(IDProgram, name.c_str()), x, y, z);
+	}
+	// ------------------------------------------------------------------------
+	void setVec4(const std::string& name, const glm::vec4& value) const
+	{
+		glUniform4fv(glGetUniformLocation(IDProgram, name.c_str()), 1, &value[0]);
+	}
+	void setVec4(const std::string& name, float x, float y, float z, float w)
+	{
+		glUniform4f(glGetUniformLocation(IDProgram, name.c_str()), x, y, z, w);
+	}
+	// ------------------------------------------------------------------------
+	void setMat2(const std::string& name, const glm::mat2& mat) const
+	{
+		glUniformMatrix2fv(glGetUniformLocation(IDProgram, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+	// ------------------------------------------------------------------------
+	void setMat3(const std::string& name, const glm::mat3& mat) const
+	{
+		glUniformMatrix3fv(glGetUniformLocation(IDProgram, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+	// ------------------------------------------------------------------------
+	void setMat4(const std::string& name, const glm::mat4& mat) const
+	{
+		glUniformMatrix4fv(glGetUniformLocation(IDProgram, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+
+private:
+	
+	//khởi tạo 1 hàm cho việc check quá trình compile,linking shader, 
+	void CheckCompileError(GLuint shader, std::string type)   //type: VERTEX,FRAGMENT,PROGRAM
+	{
+		int success;
+		char infolog[1024];
+		if (type != "PROGRAM")
+		{
+			//check compile shader
+			glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+			if (!success)
+			{
+				glGetShaderInfoLog(shader, 1024, NULL, infolog);
+				std::cout << "ERROR: COMPILE SHADER :" << "\n" << infolog << std::endl;
+			}
+		}
+		else
+		{
+			glGetProgramiv(shader, GL_LINK_STATUS, &success);
+			if (!success)
+			{
+				glGetProgramInfoLog(shader, 1024, NULL, infolog);
+				std::cout << "ERROR: LINKING PROGRAM :" << "\n" << infolog << std::endl;
+			}
+		}
+	}
+
+};
+#endif
